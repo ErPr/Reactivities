854913a7d146819d891766e0c9e60dae937ba4af
Eric Price <wunderkammer123@gmail.com>
2019-09-09 19:35:27 -0400

    Create database, add seed data

--- API/Program.cs
+++ API/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -14,7 +17,26 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            //get a references to DataContext (can't inject)
+            using (var scope = host.Services.CreateScope())
+            {
+                //get reference to services
+                var services = scope.ServiceProvider;
+                //attempt to get DatabaseContext then migrate the database
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration.");
+                }
+            } 
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -22,4 +44,3 @@ namespace API
                 .UseStartup<Startup>();
     }
 }
-  
